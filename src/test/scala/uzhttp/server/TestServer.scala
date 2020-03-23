a0af3461ed1d8dda4ea5ce54272a1db9d22435ae
Jeremy Smith <jeremys@netflix.com>
2020-03-23 08:52:45 -0700

    Add a little extra debug logging, update example
    - Debug log if selector loop gets interrupted
    - Add websocket example to `TestServer`

--- src/test/scala/uzhttp/server/TestServer.scala
+++ src/test/scala/uzhttp/server/TestServer.scala
@@ -4,11 +4,13 @@ import java.net.{InetSocketAddress, URLConnection}
 import java.nio.file.Paths
 import java.util.concurrent.TimeUnit
 
+import uzhttp.websocket.{Binary, Close, Continuation, Frame, Ping, Pong, Text}
 import uzhttp.{HTTPError, Request, Response}
-import zio.{RIO, ZIO}
+import zio.{RIO, UIO, ZIO}
 import zio.blocking.Blocking
 import zio.duration.Duration
 import zio.internal.{Platform, Tracing}
+import zio.stream.{Take, Stream}
 
 object TestServer extends zio.App {
   private lazy val resourcePath = Paths.get(getClass.getClassLoader.getResource("site").toURI)
@@ -54,15 +56,33 @@ object TestServer extends zio.App {
   private def responseCache(args: List[String]) =
     Response.permanentCache.handleAll(requestHandler(args)).build
 
+  private def log(str: String): UIO[Unit] = ZIO.effectTotal(System.err.println(s"[APP]     $str"))
+
+  private def formatHex(bytes: Array[Byte]) = bytes.map(b => String.format("%02x", Byte.box(b))).mkString
+
+  private def handleWebsocketFrame(frame: Frame): UIO[Stream[Nothing, Take[Nothing, Frame]]] = frame match {
+    case frame@Binary(data, _)       => log(s"Binary frame: 0x${formatHex(data)}") as Stream(Take.Value(frame))
+    case frame@Text(data, _)         => log(s"Text frame: $data") as Stream(Take.Value(frame))
+    case frame@Continuation(data, _) => log(s"Continuation frame: 0x${formatHex(data)}") as Stream(Take.Value(frame))
+    case Ping => log("Ping!") as Stream(Take.Value(Pong))
+    case Pong => log("Pong!") as Stream.empty
+    case Close => log("Close") as Stream(Take.Value(Close), Take.End)
+  }
 
   override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
     responseCache(args).use {
       cache =>
         Server.builder(new InetSocketAddress("127.0.0.1", 9121))
+          .handleSome {
+            case req @ Request.WebsocketRequest(_, uri, _, _, inputFrames) if uri.getPath startsWith "/ws" =>
+              for {
+                ws    <- Response.websocket(req, Stream.flatten(inputFrames.mapM(handleWebsocketFrame)).unTake)
+              } yield ws
+          }
         .handleSome(cache)
         .withMaxPending(Short.MaxValue)
         .withLogger(ServerLogger.Silent)
-        //.withLogger(ServerLogger.Debug)
+        .withLogger(ServerLogger.Debug)
         //.withConnectionIdleTimeout(Duration(5, TimeUnit.SECONDS))
         .serve.use {
           server => server.awaitShutdown.as(0)
